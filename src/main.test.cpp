b419067faf2a7fec308c5b4252d89a083f479a4e
nfarid <54642193+nfarid@users.noreply.github.com>
2022-02-13 00:35:00 +0000

    Added Drogon's event loop for tests.

--- src/main.test.cpp
+++ src/main.test.cpp
@@ -3,6 +3,26 @@
 #define DROGON_TEST_MAIN
 #include <drogon/drogon_test.h>
 
+#include "./app.hpp"
+
 int main(int argc, char** argv) {
-    return drogon::test::run(argc, argv);
+    auto& app = get_app();
+    std::promise<void> promise;
+    std::future<void> future = promise.get_future();
+
+    // Start the main loop on another thread
+    std::thread thr([& app, & promise](){
+        // Queues the promis to be fulfilled after starting the loop
+        app.getLoop()->queueInLoop([& promise](){ promise.set_value(); });
+        app.run();
+    });
+
+    // The future is only satisfied after the event loop started
+    future.get();
+    const int status = drogon::test::run(argc, argv);
+
+    // Ask the event loop to shutdown and wait
+    app.getLoop()->queueInLoop([& app](){ app.quit(); });
+    thr.join();
+    return status;
 }
