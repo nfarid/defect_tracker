d3409a2b2daa6719f1bd8682e65d85f0f076e4ea
nfarid <54642193+nfarid@users.noreply.github.com>
2022-02-13 00:12:57 +0000

    Misc edits.

--- src/main.test.cpp
+++ src/main.test.cpp
@@ -3,20 +3,6 @@
 #define DROGON_TEST_MAIN
 #include <drogon/drogon_test.h>
 
-int sum_all(int n) {
-    int result = 0;
-    for(int i=1; i<=n; i++)
-        result += i;
-    return result;
-}
-
-DROGON_TEST(Sum){
-    CHECK(sum_all(0) == 0);
-    CHECK(sum_all(1) == 1);
-    CHECK(sum_all(2) == 3);
-    CHECK(sum_all(3) == 6);
-}
-
 int main(int argc, char** argv) {
     return drogon::test::run(argc, argv);
 }
