136d857ae1442babd981640ca6040833599383a5
nfarid <54642193+nfarid@users.noreply.github.com>
2022-02-13 00:45:44 +0000

    Added simple test.

---
+++ src/controllers/home.test.cpp
@@ -0,0 +1,20 @@
+
+#include <drogon/HttpClient.h>
+#include <drogon/drogon_test.h>
+
+using namespace drogon;
+
+DROGON_TEST(RemoteAPITest){
+    auto client = HttpClient::newHttpClient("http://localhost:3000");
+    auto req = HttpRequest::newHttpRequest();
+    req->setPath("/");
+    client->sendRequest(req, [TEST_CTX](ReqResult res, const HttpResponsePtr& resp){
+        // There's nothing we can do if the request didn't reach the server
+        // or the server generated garbage.
+        REQUIRE(res == ReqResult::Ok);
+        REQUIRE(resp != nullptr);
+
+        CHECK(resp->getStatusCode() == k200OK);
+        CHECK(resp->contentType() == CT_TEXT_HTML);
+    });
+}
