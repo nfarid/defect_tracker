40a154db0505873c69894a8c88b011c478c74529
nfarid <54642193+nfarid@users.noreply.github.com>
2022-02-18 20:02:56 +0000

    Edited login.

--- src/controllers/session.cpp
+++ src/controllers/session.cpp
@@ -1,14 +1,19 @@
 
 #include "./session.hpp"
+#include "../util/core.hpp"
 
 #include <sodium.h>
 
 #include <array>
 #include <iostream>
+#include <string>
 
 
 namespace Ctrlr
 {
+
+
+using std::string_literals::operator""s;
 
 
 void Session::newGet(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& cb) {
@@ -31,16 +36,21 @@ void Session::create(const HttpRequestPtr& req, std::function<void(const HttpRes
     // Check if the username and password has been entered
     const auto usernameIter = postParams.find("login_username");
     if(usernameIter == end(postParams) ) {
-        data.insert("error", "Username has not been entered");
-        return cb(HttpResponse::newRedirectionResponse("/login") );
+        data.insert("login_error", "Username has not been entered"s);
+        return cb(HttpResponse::newHttpViewResponse("login.csp", data) );
     }
     const auto& username = usernameIter->second;
     const auto passwordIter = postParams.find("login_password");
     if(passwordIter == end(postParams) ) {
-        data.insert("error", "Password has not been entered");
-        return cb(HttpResponse::newRedirectionResponse("/login") );
+        data.insert("login_error", "Password has not been entered"s);
+        return cb(HttpResponse::newHttpViewResponse("login.csp", data) );
     }
     const auto& password = passwordIter->second;
+
+    // Check if the username & password is correct
+
+    const Criteria userCriteria{Model::Account::Cols::_username, CompareOperator::EQ, username};
+    auto userFuture = mAccountOrm.findFutureBy(userCriteria);
 
     std::array<char, crypto_pwhash_STRBYTES> passwordHash{};
     if(crypto_pwhash_str(passwordHash.data(), password.c_str(), size(password),
@@ -50,16 +60,31 @@ void Session::create(const HttpRequestPtr& req, std::function<void(const HttpRes
         std::abort();
     }
 
-    const Criteria criteria{Model::Account::Cols::_username, CompareOperator::EQ, username};
     try {
-        const Model::Account user = mAccountOrm.findOne(criteria);
-        req->session()->insert("user", user.getValueOfId() );
-    } catch(std::exception& ex) {
-        data.insert("error", "Invalid username or password");
-        return cb(HttpResponse::newRedirectionResponse("/login") );
-    }
+        const auto& foundUserLst = userFuture.get();
+        if(foundUserLst.empty() ) {
+            // No user with that name is in the datbase i.e. incorrect username
+            data.insert("login_error", "Incorrect username"s);
+            return cb(HttpResponse::newHttpViewResponse("login.csp", data) );
+        }
+        const auto& user = foundUserLst.front();
 
-    return cb(HttpResponse::newRedirectionResponse("/") );
+        if(user.getValueOfPasswordDigest() != passwordHash.data() ) {
+            // Incorrect password
+            data.insert("login_error", "Incorrect password"s);
+            data.insert("login_username", username);
+            return cb(HttpResponse::newHttpViewResponse("login.csp", data) );
+        }
+
+        // Everything is correct, so the user can login
+        req->session()->insert("user", user.getValueOfId() );
+        return cb(HttpResponse::newRedirectionResponse("/") );
+    } catch(std::exception& ex) {
+        // TODO: Better error handling
+        std::cerr<<ex.what()<<std::endl;
+        std::abort();
+    }
+    UTIL_UNREACHABLE();
 }
 
 
