731087f971585a0f52898e4b3068a50d00fc78d0
nfarid <54642193+nfarid@users.noreply.github.com>
2022-02-22 20:07:25 +0000

    Edited error handling.

--- src/controllers/home.cpp
+++ src/controllers/home.cpp
@@ -2,6 +2,7 @@
 #include "./home.hpp"
 
 #include <string>
+#include <iostream>
 
 
 namespace Ctrlr
@@ -13,8 +14,10 @@ using std::string_literals::operator""s;
 
 void Home::index(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& cb) {
     const auto session = req->session();
-    if(!session)
-        std::abort();// TODO: Change this.
+    if(!session) {
+        std::cerr<<"Session is not enabled"<<std::endl;
+        throw std::runtime_error("Session is not enabled");
+    }
 
     const auto userId = session->getOptional<int32_t>("user");
     std::optional<std::string> username{};
--- src/controllers/session.cpp
+++ src/controllers/session.cpp
@@ -17,8 +17,10 @@ using std::string_literals::operator""s;
 
 void Session::newGet(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& cb) {
     const auto session = req->session();
-    if(!session)
-        std::abort();
+    if(!session) {
+        std::cerr<<"Session is not enabled"<<std::endl;
+        throw std::runtime_error("Session is not enabled");
+    }
     const auto userId = session->getOptional<int32_t>("user");
 
     // If the user has already logged in, there's no point of the login page.
