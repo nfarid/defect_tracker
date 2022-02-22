5aedba86111d01227fc6e8a30af4329eec9afb38
nfarid <54642193+nfarid@users.noreply.github.com>
2022-02-22 21:14:10 +0000

    Added tests for session controller.

--- src/controllers/session.test.cpp
+++ src/controllers/session.test.cpp
@@ -1,11 +1,22 @@
 
+#include "../constants.hpp"
+
 #include <drogon/HttpClient.h>
 #include <drogon/drogon_test.h>
 
-#include <iostream>
-
 using namespace drogon;
 
-DROGON_TEST(SessionControllerTest){
-//
+DROGON_TEST(SessionController_LoginGet){
+    auto client = HttpClient::newHttpClient(localhost, testPort);
+
+    auto req = HttpRequest::newHttpRequest();
+    req->setPath("/login");
+
+    client->sendRequest(req, [TEST_CTX](ReqResult res, const HttpResponsePtr& resp){
+        REQUIRE(res == ReqResult::Ok);
+        REQUIRE(resp != nullptr);
+
+        CHECK(resp->getStatusCode() == k200OK);
+        CHECK(resp->contentType() == CT_TEXT_HTML);
+    });
 }
