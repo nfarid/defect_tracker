54e3d840fab9f774f8944a44c0f7a32520d83d55
nfarid <54642193+nfarid@users.noreply.github.com>
2022-02-25 20:37:08 +0000

    Created create_test_data.sql.

---
+++ tools/create_test_data.sql
@@ -0,0 +1,12 @@
+
+--For creating some test data.
+
+--First delete any data already in the database
+DELETE FROM Account;
+DELETE FROM Project;
+DELETE FROM Ticket;
+DELETE FROM Staff;
+DELETE FROM Comment;
+
+--Then add the test data
+INSERT INTO Account(username, password_digest) VALUES ('username', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME');
