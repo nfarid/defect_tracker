ba14011e0e352ed8cfcc8080e555cefd5f0d6ec2
nfarid <54642193+nfarid@users.noreply.github.com>
2022-03-02 20:55:19 +0000

    Added a simple project page.

---
+++ src/controllers/project.cpp
@@ -0,0 +1,48 @@
+
+#include "./auxiliary.hpp"
+#include "../models/account.hpp"
+#include "../models/project.hpp"
+
+#include <drogon/HttpController.h>
+
+
+namespace Ctrlr
+{
+
+
+using namespace Aux;
+using namespace drogon;
+using namespace drogon::orm;
+
+class Project : public HttpController<Project> {
+public:
+    /*NO-FORMAT*/
+    METHOD_LIST_BEGIN
+        ADD_METHOD_TO(Project::show, "/project/{1}", Get);
+    METHOD_LIST_END
+    /*YES-FORMAT*/
+
+    void show(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& cb, int32_t id);
+
+private:
+    Mapper<Model::Account> mAccountOrm = Mapper<Model::Account>(app().getDbClient("db") );
+    Mapper<Model::Project> mProjectOrm = Mapper<Model::Project>(app().getDbClient("db") );
+};
+
+void Project::show(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& cb, int32_t id)
+{
+    try {
+        const auto project = mProjectOrm.findByPrimaryKey(id);
+        const auto manager = mAccountOrm.findByPrimaryKey(project.getValueOfManagerId() );
+        HttpViewData data = getViewData(project.getValueOfProjectName(), *getSession(req) );
+        data.insert("manager_name", manager.getValueOfUsername() );
+        return cb(HttpResponse::newHttpViewResponse("project.csp", data) );
+    } catch(std::exception& ex) {
+        std::cerr<<ex.what()<<std::endl;
+        return cb(HttpResponse::newNotFoundResponse() );
+    }
+}
+
+
+}// namespace Ctrlr
+
--- tools/create_test_data.sql
+++ tools/create_test_data.sql
@@ -10,3 +10,5 @@ DELETE FROM Comment;
 
 --Then add the test data
 INSERT INTO Account(username, password_digest) VALUES ('username', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME');
+--TODO Add a project for the test data
+INSERT INTO Project(project_name, manager_id) VALUES ('project', (SELECT id FROM Account WHERE username='username') );
