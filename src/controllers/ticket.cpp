6f0480d2808da75b3b6e7245c8eb2402fc2ce4d0
nfarid <54642193+nfarid@users.noreply.github.com>
2022-03-11 23:29:33 +0000

    Created simple ticket form.

--- src/controllers/ticket.cpp
+++ src/controllers/ticket.cpp
@@ -1,6 +1,7 @@
 
 #include "./auxiliary.hpp"
 #include "../constants.hpp"
+#include "../models/project.hpp"
 #include "../models/ticket.hpp"
 #include "../models/comment.hpp"
 
@@ -21,10 +22,15 @@ public:
     /*NO-FORMAT*/
     METHOD_LIST_BEGIN
         ADD_METHOD_TO(Ticket::show, "/ticket/{1}", Get);
+        ADD_METHOD_TO(Ticket::newForm, "/project/{1}/report", Get);
     METHOD_LIST_END
     /*YES-FORMAT*/
 
     void show(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id);
+    void newForm(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id);
+
+private:
+    Mapper<Model::Project> mProjectOrm = Mapper<Model::Project>(app().getDbClient("db") );
     Mapper<Model::Ticket> mTicketOrm = Mapper<Model::Ticket>(app().getDbClient("db") );
     Mapper<Model::Comment> mCommentOrm = Mapper<Model::Comment>(app().getDbClient("db") );
 };
@@ -33,11 +39,14 @@ void Ticket::show(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id) 
     try {
         // TODO:
         auto ticketFuture = mTicketOrm.findFutureByPrimaryKey(id);
+        auto projectFuture = mProjectOrm.findFutureByPrimaryKey(id);
         const Criteria commentCriteria{Model::Comment::Cols::_project, CompareOperator::EQ, id};
         auto commentLstFuture = mCommentOrm.findFutureBy(commentCriteria);
 
         const auto ticket = ticketFuture.get();
         const auto commentLst = commentLstFuture.get();
+        const auto project = projectFuture.get();
+
         std::vector<Json::Value> commentLstJson{};
         commentLstJson.reserve(size(commentLst) );
         for(const auto& comment : commentLst) {
@@ -51,12 +60,23 @@ void Ticket::show(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id) 
         auto data = getViewData(ticket.getValueOfTitle(), *getSession(req) );
         data.insert("ticket", std::move(ticketJson) );
         data.insert("commentLst", std::move(commentLstJson) );
+        data.insert("project_name", project.getValueOfProjectName() );
         return cb(HttpResponse::newHttpViewResponse("ticket.csp", data) );
     } catch(const std::exception& ex) {
         std::cerr<<ex.what()<<std::endl;
         return cb(HttpResponse::newNotFoundResponse() );
     }
 }
+
+void Ticket::newForm(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id) {
+    try {
+        auto projectFuture = mProjectOrm.findFutureByPrimaryKey(id);
+        auto data = getViewData("New Ticket", *getSession(req) );
+        const auto project = projectFuture.get();
+        data.insert("project", project.toJson() );
+        return cb(HttpResponse::newHttpViewResponse("ticket_form.csp", data) );
+    }  catch(const std::exception& ex) {
+        std::cerr<<ex.what()<<std::endl;
         return cb(HttpResponse::newNotFoundResponse() );
     }
 }
