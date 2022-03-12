2af6f97f7a7dc33752c5cae67f977f8ad86184d2
nfarid <54642193+nfarid@users.noreply.github.com>
2022-03-12 14:36:23 +0000

    Edited report page.

--- src/controllers/ticket.cpp
+++ src/controllers/ticket.cpp
@@ -1,9 +1,10 @@
 
 #include "./auxiliary.hpp"
 #include "../constants.hpp"
+#include "../models/auxiliary.hpp"
+#include "../models/comment.hpp"
 #include "../models/project.hpp"
 #include "../models/ticket.hpp"
-#include "../models/comment.hpp"
 
 #include <drogon/HttpController.h>
 
@@ -23,11 +24,13 @@ public:
     METHOD_LIST_BEGIN
         ADD_METHOD_TO(Ticket::show, "/ticket/{1}", Get);
         ADD_METHOD_TO(Ticket::newForm, "/project/{1}/report", Get);
+        ADD_METHOD_TO(Ticket::create, "/project/{1}/report", Post);
     METHOD_LIST_END
     /*YES-FORMAT*/
 
     void show(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id);
     void newForm(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id);
+    void create(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id);
 
 private:
     Mapper<Model::Project> mProjectOrm = Mapper<Model::Project>(app().getDbClient("db") );
@@ -37,29 +40,29 @@ private:
 
 void Ticket::show(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id) {
     try {
-        // TODO:
+        // Obtain ticket, project and comment models
         auto ticketFuture = mTicketOrm.findFutureByPrimaryKey(id);
         auto projectFuture = mProjectOrm.findFutureByPrimaryKey(id);
         const Criteria commentCriteria{Model::Comment::Cols::_project, CompareOperator::EQ, id};
         auto commentLstFuture = mCommentOrm.findFutureBy(commentCriteria);
 
-        const auto ticket = ticketFuture.get();
+        // Convert these values to json
+        Json::Value commentLstJson;
         const auto commentLst = commentLstFuture.get();
-        const auto project = projectFuture.get();
-
-        std::vector<Json::Value> commentLstJson{};
-        commentLstJson.reserve(size(commentLst) );
         for(const auto& comment : commentLst) {
             auto commentJson = comment.toJson();
             commentJson["created_date"] = comment.getValueOfCreatedDate().toCustomedFormattedString(dateFormat);
-            commentLstJson.push_back(std::move(commentJson) );
+            commentLstJson.append(std::move(commentJson) );
         }
+        const auto ticket = ticketFuture.get();
         auto ticketJson = ticket.toJson();
         ticketJson["created_date"] = ticket.getValueOfCreatedDate().toCustomedFormattedString(dateFormat);
 
+        // Send the data to the ticket view
         auto data = getViewData(ticket.getValueOfTitle(), *getSession(req) );
         data.insert("ticket", std::move(ticketJson) );
         data.insert("commentLst", std::move(commentLstJson) );
+        const auto project = projectFuture.get();
         data.insert("project_name", project.getValueOfProjectName() );
         return cb(HttpResponse::newHttpViewResponse("ticket.csp", data) );
     } catch(const std::exception& ex) {
@@ -71,10 +74,22 @@ void Ticket::show(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id) 
 void Ticket::newForm(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id) {
     try {
         auto projectFuture = mProjectOrm.findFutureByPrimaryKey(id);
+
         auto data = getViewData("New Ticket", *getSession(req) );
         const auto project = projectFuture.get();
         data.insert("project", project.toJson() );
+        data.insert("severityLst", severityLstJson() );
+
         return cb(HttpResponse::newHttpViewResponse("ticket_form.csp", data) );
+    }  catch(const std::exception& ex) {
+        std::cerr<<ex.what()<<std::endl;
+        return cb(HttpResponse::newNotFoundResponse() );
+    }
+}
+
+void Ticket::create(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id) {
+    try {
+        auto projectFuture = mProjectOrm.findFutureByPrimaryKey(id);
     }  catch(const std::exception& ex) {
         std::cerr<<ex.what()<<std::endl;
         return cb(HttpResponse::newNotFoundResponse() );
--- src/models/auxiliary.hpp
+++ src/models/auxiliary.hpp
@@ -2,4 +2,44 @@
 #ifndef HPP_MODELS_AUXILIARY
 #define HPP_MODELS_AUXILIARY
 
-#endif // ifndef HPP_MODELS_AUXILIARY
+#include <json/value.h>
+
+#include <array>
+#include <string>
+
+
+
+namespace Aux
+{
+
+
+using std::string_literals::operator""s;
+
+const std::array statusLst = {"new"s, "confirmed"s, "unreproducible"s, "resolved"s, "reopened"s};
+// Returns a list of statuses{"new","confirmed","unreproducible","resolved","reopened"} as a json
+inline Json::Value statusLstJson() {
+    const Json::Value static statusLst_ = []{
+        Json::Value statusLst_;
+        for(const auto& status : statusLst)
+            statusLst_.append(status);
+        return statusLst_;
+    }();
+    return statusLst_;
+}
+
+const std::array severityLst = {"low"s, "medium"s, "high"s};
+// Returns a list of severities{"low","medium","high"} as a json
+inline Json::Value severityLstJson() {
+    const Json::Value static severityLst_ = []{
+        Json::Value severityLst_;
+        for(const auto& status : severityLst)
+            severityLst_.append(status);
+        return severityLst_;
+    }();
+    return severityLst_;
+}
+
+
+}  // namespace Aux
+
+#endif  // ifndef HPP_MODELS_AUXILIARY
