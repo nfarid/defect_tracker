92ca56ce76421c33ae2cceebedb4b26939d4a3e3
nfarid <54642193+nfarid@users.noreply.github.com>
2022-03-15 21:31:20 +0000

    Added Comment::Create.

--- src/controllers/comment.cpp
+++ src/controllers/comment.cpp
@@ -1,5 +1,6 @@
 
 #include "./auxiliary.hpp"
+#include "../models/comment.hpp"
 
 #include <drogon/HttpController.h>
 
@@ -19,16 +20,34 @@ public:
     /*NO-FORMAT*/
     METHOD_LIST_BEGIN
         ADD_METHOD_TO(Comment::newForm, "/ticket/{1}/comment", Get);
+        ADD_METHOD_TO(Comment::create, "/ticket/{1}/comment", Post);
     METHOD_LIST_END
     /*YES-FORMAT*/
 
-    void newForm(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t ticketId);
+    static void newForm(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t ticketId);
+    void create(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t ticketId);
+
+private:
+    Mapper<Model::Comment> mCommentOrm = Mapper<Model::Comment>(app().getDbClient("db") );
 };
 
 void Comment::newForm(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t ticketId) {
     auto data = getViewData("Post Comment", *getSession(req) );
     data.insert("ticketId", std::to_string(ticketId) );
     return cb(HttpResponse::newHttpViewResponse("comment_form.csp", data) );
+}
+
+void Comment::create(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t ticketId) {
+    const auto& postParams = req->parameters();
+    try {
+        Model::Comment newComment{};
+        newComment.setPost(postParams.at("form_post") );
+        newComment.setCreatedDate(trantor::Date::now() );
+        mCommentOrm.insert(newComment);
+    } catch(std::exception& ex) {
+        std::cerr<<ex.what()<<std::endl;
+        return cb(HttpResponse::newNotFoundResponse() );
+    }
 }
 
 
