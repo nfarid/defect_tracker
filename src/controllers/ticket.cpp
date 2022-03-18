a0197735ed06059d6215c238fd6fca31bcc5ed58
nfarid <54642193+nfarid@users.noreply.github.com>
2022-03-18 20:43:00 +0000

    Added ticket update (WIP).

--- src/controllers/ticket.cpp
+++ src/controllers/ticket.cpp
@@ -4,6 +4,7 @@
 #include "../models/auxiliary.hpp"
 #include "../models/comment.hpp"
 #include "../models/project.hpp"
+#include "../models/staff.hpp"
 #include "../models/ticket.hpp"
 
 #include <drogon/HttpController.h>
@@ -26,16 +27,21 @@ public:
     METHOD_LIST_BEGIN
         ADD_METHOD_TO(Ticket::show, "/ticket/{1}", Get);
         ADD_METHOD_TO(Ticket::newForm, "/project/{1}/report", Get);
+        ADD_METHOD_TO(Ticket::edit, "/ticket/{1}/edit", Get);
         ADD_METHOD_TO(Ticket::create, "/project/{1}/report", Post);
+        ADD_METHOD_TO(Ticket::update, "/ticket/{1}/edit", Post);
     METHOD_LIST_END
     /*YES-FORMAT*/
 
     void show(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id);
     void newForm(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id);
+    void edit(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id);
     void create(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id);
+    void update(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id);
 
 private:
     Mapper<Model::Project> mProjectOrm = Mapper<Model::Project>(app().getDbClient("db") );
+    Mapper<Model::Staff> mStaffOrm  = Mapper<Model::Staff>(app().getDbClient("db") );
     Mapper<Model::Ticket> mTicketOrm = Mapper<Model::Ticket>(app().getDbClient("db") );
     Mapper<Model::Comment> mCommentOrm = Mapper<Model::Comment>(app().getDbClient("db") );
 };
@@ -86,6 +92,57 @@ void Ticket::newForm(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t i
 
         return cb(HttpResponse::newHttpViewResponse("ticket_form.csp", data) );
     }  catch(const std::exception& ex) {
+        std::cerr<<ex.what()<<std::endl;
+        return cb(HttpResponse::newNotFoundResponse() );
+    }
+}
+
+void Ticket::edit(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id) {
+    try {
+        const auto session = getSession(req);
+        if(!isLoggedIn(*session) )
+            return cb(HttpResponse::newNotFoundResponse() );
+
+        auto ticketFuture = mTicketOrm.findFutureByPrimaryKey(id);
+        auto data = getViewData("Edit Ticket", *session);
+        data.insert("ticketId", std::to_string(id) );
+
+        const auto ticket = ticketFuture.get();
+        auto projectFuture = mProjectOrm.findFutureByPrimaryKey(ticket.getValueOfProjectId() );
+        data.insert("ticketTitle", ticket.getValueOfTitle() );
+        data.insert("ticketDescription", ticket.getValueOfDescrption() );
+        const bool isAssigned = !!ticket.getAssignedId();
+        if(isAssigned)
+            data.insert("ticketAssigned", std::to_string(ticket.getValueOfAssignedId() ) );
+
+        bool authorised = false;
+        const auto userId = session->get<int32_t>("user_id");
+        // If the user was the original reporter, then they can edit the ticket
+        if(userId == ticket.getValueOfReporterId() ) {
+            authorised = true;
+            data.insert("canEdit", true);
+        }
+
+        const auto project = projectFuture.get();
+        const Criteria staffCriteria{Model::Staff::Cols::_project_id, CompareOperator::EQ, project.getValueOfId()};
+        const auto staffLst = mStaffOrm.findBy(staffCriteria);
+        // If the user was project manager, then they can assign to a staff member
+        if(userId == project.getValueOfManagerId() ) {
+            authorised = true;
+            data.insert("canAssign", true);
+
+            Json::Value staffLstJson;
+            for(const auto& staff : staffLst)
+                staffLstJson.append(staff.toJson() );
+            data.insert("staffLst", staffLstJson);
+        }         // TODO: Allow staff to self assign
+
+        // If the user is not authorised to edit any parts of the ticket
+        if(!authorised)
+            return cb(HttpResponse::newNotFoundResponse() );
+
+        return cb(HttpResponse::newHttpViewResponse("ticket_edit.csp", data) );
+    } catch(const std::exception& ex) {
         std::cerr<<ex.what()<<std::endl;
         return cb(HttpResponse::newNotFoundResponse() );
     }
@@ -154,6 +211,32 @@ void Ticket::create(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t pr
     }
 }
 
+void Ticket::update(const HttpRequestPtr& req, ResponseCallback&& cb, int32_t id) {
+    try {
+        std::future ticketFuture = mTicketOrm.findFutureByPrimaryKey(id);
+        const auto session = req->session();
+        const auto& postParams = req->parameters();
+        Model::Ticket ticket = ticketFuture.get();
 
-}// namespace Ctrlr
+        if(!isLoggedIn(*session) )
+            return cb(HttpResponse::newNotFoundResponse() );
+
+        const int32_t userId = session->get<int32_t>("user_id");
+        // The ticket's reporter can edit the ticket
+        if(userId == ticket.getValueOfReporterId() ) {
+            ticket.setDescrption(postParams.at("ticketDescription") );
+            mTicketOrm.update(ticket);
+        }
+
+        // TODO: Manager can assign
+
+        return cb(HttpResponse::newRedirectionResponse("/") );
+    }  catch(const std::exception& ex) {
+        std::cerr<<ex.what()<<std::endl;
+        return cb(HttpResponse::newNotFoundResponse() );
+    }
+}
+
+
+}  // namespace Ctrlr
 
