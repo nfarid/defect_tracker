11dc320d2a47fc04f1ecd0125b516ed9903ebc7d
nfarid <54642193+nfarid@users.noreply.github.com>
2022-04-14 18:15:45 +0100

    Renamed tools/ folder to sql/.

---
+++ sql/create_tables.sql
@@ -0,0 +1,57 @@
+
+-- Remove everything from the database
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA public;
+
+
+-- Create the tables
+CREATE TABLE account(
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE project(
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL UNIQUE,
+    description TEXT NOT NULL,
+    manager_id INTEGER REFERENCES account(id) ON DELETE CASCADE
+);
+
+CREATE TYPE status_type AS ENUM('new', 'confirmed', 'unreproducible', 'resolved', 'reopened');
+CREATE TYPE severity_type AS ENUM ('low', 'medium', 'high');
+
+CREATE TABLE ticket(
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    status status_type NOT NULL,
+    severity severity_type NOT NULL,
+    created_date TIMESTAMP NOT NULL,
+    resolved_date TIMESTAMP,
+    image_filename TEXT,
+    reporter_id INTEGER NOT NULL REFERENCES account(id) ON DELETE CASCADE,
+    assigned_id INTEGER REFERENCES account(id) ON DELETE SET NULL,
+    project_id INTEGER NOT NULL REFERENCES project(id) ON DELETE CASCADE
+);
+
+CREATE TABLE staff(
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    project_id INTEGER NOT NULL REFERENCES project(id) ON DELETE CASCADE,
+    staff_id INTEGER NOT NULL REFERENCES account(id) ON DELETE CASCADE
+);
+
+CREATE TABLE comment(
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    post TEXT NOT NULL,
+    created_date TIMESTAMP NOT NULL,
+    ticket_id INTEGER NOT NULL REFERENCES ticket(id) ON DELETE CASCADE,
+    poster_id INTEGER NOT NULL REFERENCES account(id) ON DELETE CASCADE
+);
+
+CREATE TABLE notification(
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    summary TEXT NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES account(id) ON DELETE CASCADE,
+    ticket_id INTEGER NOT NULL REFERENCES ticket(id) ON DELETE CASCADE
+);
---
+++ sql/create_test_data.sql
@@ -0,0 +1,55 @@
+
+--For creating some test data.
+
+--First delete any data already in the database
+DELETE FROM Account;
+DELETE FROM Project;
+DELETE FROM Ticket;
+DELETE FROM Staff;
+DELETE FROM Comment;
+
+
+-- Create the example data
+INSERT INTO account(username, password_hash) VALUES
+    ('username', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME'),
+    ('example_manager', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME'),
+    ('example_staff_1', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME'),
+    ('example_staff_2', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME'),
+    ('example_staff_3', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME'),
+    ('example_user', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME'),
+    ('demo_regular_user', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME'),
+    ('demo_project_staff', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME'),
+    ('demo_project_manager', '$argon2id$v=19$m=262144,t=3,p=1$6StGe1EqVL4n7g8cKNtl3g$zt2IXeni0yr2mF97dyHAWHJVb4gAs9QOHvEZ3iyoiME')
+;
+
+INSERT INTO project(title, description, manager_id) VALUES
+    ('project', 'description', (SELECT id FROM account WHERE username='username') ),
+    ('example_project_1', 'This is an example description.', (SELECT id FROM account WHERE username='example_manager') ),
+    ('example_project_2', 'Another example description!', (SELECT id FROM account WHERE username='example_manager') ),
+    ('demo_project', 'This is a demo project.', (SELECT id FROM account WHERE username='example_manager') )
+;
+
+INSERT INTO ticket(title, description, status, severity, created_date, resolved_date, assigned_id, project_id, reporter_id) VALUES
+    ('ticket', 'description', 'new', 'low', CURRENT_TIMESTAMP, NULL, NULL, (SELECT id FROM project WHERE title='project'), (SELECT id FROM account WHERE username='example_user') ),
+    ('example_ticket_1', 'example_description', 'new', 'low', CURRENT_TIMESTAMP, NULL, NULL, (SELECT id FROM project WHERE title='example_project_1'), (SELECT id FROM account WHERE username='example_user') ),
+    ('example_ticket_2', 'another_description', 'confirmed', 'high', CURRENT_TIMESTAMP, NULL, (SELECT id FROM account WHERE username='example_staff_1'), (SELECT id FROM project WHERE title='example_project_1'), (SELECT id FROM account WHERE username='example_user') ),
+    ('demo_ticket_1', 'this is the first demo ticket', 'new', 'high', CURRENT_TIMESTAMP, NULL, NULL, (SELECT id FROM project WHERE title='demo_project'), (SELECT id FROM account WHERE username='demo_regular_user') ),
+    ('demo_ticket_2', 'this is the second demo ticket', 'confirmed', 'medium', CURRENT_TIMESTAMP, NULL, (SELECT id FROM account WHERE username='demo_project_staff'), (SELECT id FROM project WHERE title='demo_project'), (SELECT id FROM account WHERE username='demo_regular_user') ),
+    ('demo_ticket_3', 'this is the third demo ticket', 'confirmed', 'high', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL, (SELECT id FROM project WHERE title='demo_project'), (SELECT id FROM account WHERE username='demo_regular_user') )
+;
+
+
+INSERT INTO staff(project_id, staff_id) VALUES
+    ( (SELECT id FROM project WHERE title='example_project_1'), (SELECT id FROM account WHERE username='example_staff_1') ),
+    ( (SELECT id FROM project WHERE title='example_project_1'), (SELECT id FROM account WHERE username='example_staff_2') ),
+    ( (SELECT id FROM project WHERE title='example_project_1'), (SELECT id FROM account WHERE username='example_staff_3') ),
+    ( (SELECT id FROM project WHERE title='example_project_2'), (SELECT id FROM account WHERE username='example_staff_1') ),
+    ( (SELECT id FROM project WHERE title='demo_project'), (SELECT id FROM account WHERE username='demo_project_staff') )
+;
+
+INSERT INTO comment(post, created_date, ticket_id, poster_id) VALUES
+    ('This is an example post', CURRENT_TIMESTAMP, (SELECT id FROM ticket WHERE title='example_ticket_1'), (SELECT id FROM account WHERE username='username') ),
+    ('Confirmed', CURRENT_TIMESTAMP, (SELECT id FROM ticket WHERE title='demo_ticket_2'), (SELECT id FROM account WHERE username='demo_project_manager') ),
+    ('Resolved', CURRENT_TIMESTAMP, (SELECT id FROM ticket WHERE title='demo_ticket_3'), (SELECT id FROM account WHERE username='demo_project_staff') ),
+    ('This is resolved', CURRENT_TIMESTAMP, (SELECT id FROM ticket WHERE title='demo_ticket_3'), (SELECT id FROM account WHERE username='demo_project_manager') )
+;
