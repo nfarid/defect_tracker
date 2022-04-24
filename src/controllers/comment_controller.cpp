5e9a7fa873fe964517e02f54ea3e3bd6ca51dae2
nfarid <54642193+nfarid@users.noreply.github.com>
2022-04-24 22:26:22 +0100

    Added comments to comment controller.

--- src/controllers/comment_controller.cpp
+++ src/controllers/comment_controller.cpp
@@ -18,6 +18,7 @@ namespace Ctrlr
 using namespace Aux;
 using namespace drogon;
 using namespace drogon::orm;
+using Model::Comment;
 using std::string_literals::operator""s;
 using std::string;
 
@@ -26,16 +27,17 @@ public:
     /*NO-FORMAT*/
     METHOD_LIST_BEGIN
         ADD_METHOD_TO(CommentController::newForm, "/ticket/{1}/comment", Get, "Fltr::OnlyLoggedIn");
-        ADD_METHOD_TO(CommentController::create, "/ticket/{1}/comment", Post, "Fltr::OnlyLoggedIn", "Fltr::ValidToken");
+        ADD_METHOD_TO(CommentController::createSubmit, "/ticket/{1}/comment", Post, "Fltr::OnlyLoggedIn", "Fltr::ValidToken");
     METHOD_LIST_END
     /*YES-FORMAT*/
 
+    // newForm is called whenever the user requests for a form for a new comment
     Task<HttpResponsePtr> newForm(HttpRequestPtr req, IdType ticketId);
-    Task<HttpResponsePtr> create(HttpRequestPtr req, IdType ticketId);
+
+    // createSubmit is called whenever the user submits the form data to create a ticket
+    Task<HttpResponsePtr> createSubmit(HttpRequestPtr req, IdType ticketId);
 
 private:
-    CoroMapper<Model::Comment> mCommentOrm{Util::getDb()};
-
     HttpResponsePtr newImpl(HttpRequestPtr req, IdType ticketId, string errorMessage);
 };
 
@@ -44,25 +46,24 @@ Task<HttpResponsePtr> CommentController::newForm(HttpRequestPtr req, IdType tick
 }
 
 HttpResponsePtr CommentController::newImpl(HttpRequestPtr req, IdType ticketId, string errorMessage) {
-    auto data = getViewData("Post Comment", *getSession(req) );
-    data.insert("ticket-id", std::to_string(ticketId) );
-    data.insert("form-error", errorMessage);
-    return HttpResponse::newHttpViewResponse("comment_new.csp", data);
+    auto viewData = getViewData("Post Comment", *getSession(req) );
+    viewData.insert("ticket-id", std::to_string(ticketId) );
+    viewData.insert("form-error", errorMessage);
+
+    return HttpResponse::newHttpViewResponse("comment_new.csp", viewData);
 }
 
-Task<HttpResponsePtr> CommentController::create(HttpRequestPtr req, IdType ticketId)
+Task<HttpResponsePtr> CommentController::createSubmit(HttpRequestPtr req, IdType ticketId)
 {
     const SessionPtr session = getSession(req);
     const IdType userId = getUserId(*session);
 
     const auto& postParams = req->parameters();
     try {
-        co_await Model::Comment::createComment(postParams, userId, ticketId);
-        co_return HttpResponse::newRedirectionResponse("/");
+        co_await Comment::createComment(postParams, userId, ticketId);
+        co_return HttpResponse::newRedirectionResponse("/"s + std::to_string(ticketId) );  // Redirects to the ticket page once done
     }  catch(const Util::FormError& ex) {
-        // TODO: Let the user retry
-        std::cerr<<__PRETTY_FUNCTION__<<" ; "<<__LINE__<<"\n"<<ex.what()<<std::endl;
-        co_return newImpl(req, ticketId, ex.what() );
+        co_return newImpl(req, ticketId, ex.what() );  // Retry the form if there's a form error
     } catch(const std::exception& ex) {
         std::cerr<<__PRETTY_FUNCTION__<<" ; "<<__LINE__<<"\n"<<ex.what()<<std::endl;
         co_return HttpResponse::newNotFoundResponse();
